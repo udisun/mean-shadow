d79f4a44350f47660427bea1dc5f7ee7a37aeefe
ehud shahak <udisun@gmail.com>
2014-03-16 23:06:02 +0200

    First Commit

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ mean-shadow
 ===========
 
 This module goes hand in hand with the mean.io stack and allows any mean app to have endpoints to add/remove entities from any third party app.
+
+MEAN is a fullstack javascript platform for modern web applications.
+MEAN stands for MongoDB, ExpressJS, AngularJS and Node.js.
+You can find it and all the details regarding it in:
+http://www.mean.io/
+
+After installing this module you should require it by doing:
+var meanShadow = require('../node_modules/mean-shadow')(app, auth);
+
+Then in order to add your own logic you can catch a shadow event by doing:
+meanShadow.events.on('insert', function(data) {
+  // Do some cool and MEAN stuff
+});
---
+++ mean-shadow.js
@@ -0,0 +1,132 @@
+var mongoose = require('mongoose'),
+    _ = require('lodash'),
+    db = mongoose.connections[0].db,
+    EventEmitter = require('events').EventEmitter,
+    shadowEvents = new EventEmitter();
+
+var collections = {};
+
+module.exports = function (app, auth) {
+    var meanShadow = {
+        events: shadowEvents,
+        insert: function (req, res) {
+            var name = req.params.type;
+            var query = {};
+            query[req.params.id] = req.params.val;
+
+            var Collection = getCollection(name);
+
+            Collection.findOne(query, function (err, doc) {
+                if (err) {
+                    res.send(err);
+                }
+                else if (doc) {
+                    compare(doc, req.body, function (equals) {
+                        if (!equals) {
+                            doc = _.assign(doc, req.body);
+                            Collection.save(doc, function (err, doc) {
+                                shadowEvents.emit('update', {
+                                    data: req.body,
+                                    error: err
+                                });
+
+                                res.send({
+                                    err: err,
+                                    doc: doc
+                                });
+                            });
+                        } else {
+                            shadowEvents.emit('update', {
+                                data: req.body,
+                                error: "exists"
+                            });
+
+                            res.send("equals");
+                        }
+                    });
+                } else {
+                    Collection.save(req.body, function (err, doc) {
+                        shadowEvents.emit('insert', {
+                            data: req.body,
+                            error: err
+                        });
+
+                        res.send({
+                            err: err,
+                            doc: doc
+                        });
+                    });
+                }
+            });
+        },
+
+        remove: function (req, res) {
+            var name = req.params.type;
+            var query = {};
+            query[req.params.id] = req.params.val;
+
+            var Collection = getCollection(name);
+            Collection.remove(query, function (err) {
+                if (err) {
+                    res.send(err);
+                }
+
+                shadowEvents.emit('remove', {
+                    data: query
+                });
+            });
+        }
+    };
+
+    app.post('/insert/:type/:id/:val', auth.hasAuthorization, meanShadow.insert);
+    app.del('/delete/:type/:id/:val', auth.hasAuthorization, meanShadow.remove);
+
+    return meanShadow;
+};
+
+function compare(a, b, callback) {
+    var temp = {
+        a: {},
+        b: {}
+    };
+
+    temp.a._id = a._id;
+    temp.a._v = a._v;
+    temp.b._id = b._id;
+    temp.b._v = b._v;
+
+    delete a._id;
+    delete a._v;
+
+    delete b._id;
+    delete b._v;
+
+    var equals = _.isEqual(a, b);
+
+    if (temp.a._id) {
+        a._id = temp.a._id;
+    }
+    if (temp.a._v !== 'undefined') {
+        a._v = temp.a._v;
+    }
+    if (temp.b._id) {
+        b._id = temp.b._id;
+    }
+    if (temp.b._v !== 'undefined') {
+        b._v = temp.b._v;
+    }
+
+    if (!callback) {
+        return equals;
+    }
+
+    return callback(equals);
+}
+
+function getCollection(name) {
+    if (!collections[name]) {
+        collections[name] = db.collection(name);
+    }
+
+    return collections[name];
+}
